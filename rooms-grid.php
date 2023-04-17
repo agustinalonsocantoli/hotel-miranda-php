8256e992ce5f91bc0b4a19a2ef735b80728118d9
agustinalonsocantoli <agustinalonsocantoli@gmail.com>
2023-04-17 10:30:11 +0200

    Load new code

---
+++ rooms-grid.php
@@ -0,0 +1,49 @@
+<?php include 'includes/header.php'; ?>
+
+<div class="rooms-grid">
+    <div class="rooms-grid__title">
+        <h3>THE ULTIMATE LUXURY</h3>
+
+        <h1>Ultimate Room</h1>
+
+        <div class="rooms-grid__box">
+            <h4>Home<strong> | </strong><span>Rooms</span></h4>
+        </div>
+    </div>
+
+    <div class="rooms-grid__select">
+        <select>
+            <option selected="true" disabled>Order By</option>
+            <option>Low price</option>
+            <option>High price</option>
+        </select>
+    </div>
+
+    <div class="rooms-page1 view__rooms">
+
+    </div>
+
+    <div class="rooms-page2 view__rooms">
+
+    </div>
+
+    <div class="rooms-page3 view__rooms">
+
+    </div>
+
+    <div class="rooms-page4 view__rooms">
+
+    </div>
+
+    <div class="rooms-grid__pagination">
+        <p class="btn_room-prev">«</p>
+        <p>1</p>
+        <p>2</p>
+        <p>3</p>
+        <p>4</p>
+        <p class="btn_room-next">»</p>
+    </div>
+
+</div>
+
+<?php include 'includes/footer.php'; ?>
---
+++ rooms-list.php
@@ -0,0 +1,49 @@
+<?php include 'includes/header.php'; ?>
+
+<div class="rooms-list">
+    <div class="rooms-list__title">
+        <h3>THE ULTIMATE LUXURY</h3>
+
+        <h1>Ultimate Room</h1>
+
+        <div class="rooms-list__box">
+            <h4>Home<strong> | </strong><span>Rooms</span></h4>
+        </div>
+    </div>
+
+    <div class="rooms-list__select">
+        <select>
+            <option selected="true" disabled>Order By</option>
+            <option>Low price</option>
+            <option>High price</option>
+        </select>
+    </div>
+
+    <div class="list-page1 rooms-list__view">
+
+    </div>
+
+    <div class="list-page2 rooms-list__view">
+
+    </div>
+
+    <div class="list-page3 rooms-list__view">
+
+    </div>
+
+    <div class="list-page4 rooms-list__view">
+
+    </div>
+
+    <div class="rooms-list__pagination">
+        <p class="btn_room-prev">«</p>
+        <p>1</p>
+        <p>2</p>
+        <p>3</p>
+        <p>4</p>
+        <p class="btn_room-next">»</p>
+    </div>
+
+</div>
+
+<?php include 'includes/footer.php'; ?>
