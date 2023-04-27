65f5aec79dca5559146b041ae7d12f52ffc102e7
agustinalonsocantoli <agustinalonsocantoli@gmail.com>
2023-04-27 10:16:15 +0200

    blade

---
+++ views/about.blade.php
@@ -0,0 +1,280 @@
+@extends('layout')
+
+@section('content')
+<div class="about">
+    <div class="about__title">
+        <h3>THE ULTIMATE LUXURY</h3>
+
+        <h1>About Us</h1>
+
+        <div class="about__box">
+            <h4>Home<strong> | </strong><span>About</span></h4>
+        </div>
+    </div>
+
+    <div class="about__services">
+        
+        <div class="about__video">
+            <video src="src/vid/hotel.mp4" type="video/mp4" controls autoplay muted></video>
+        </div>
+
+        <h4>
+            Hello. Our hotel has been present for over 20 years. We make the best for all our customers.
+        </h4>
+
+        <div class="about-services">
+            <div>
+                <img src="src/img/icons/breakfast.svg" alt="breakfast">
+                <p>Breakfast</p>
+            </div>
+
+            <div class="airport">
+                <img src="src/img/icons/airport.svg" alt="airport">
+                <p>Airport Pickup</p>
+            </div>
+
+            <div>
+                <img src="src/img/icons/guide.svg" alt="guide">
+                <p>City Guide</p>
+            </div>
+
+            <div class="services-desktop">
+                <img src="src/img/icons/bbq.svg" alt="bbq">
+                <p>BBQ Party</p>
+            </div>
+
+            <div>
+                <img src="src/img/icons/room.svg" alt="room">
+                <p>Luxury Room</p>
+            </div>
+
+        </div>
+    </div>
+
+    <div class="about__rest">
+        <div class="rest">
+            <div class="rest-img">
+                <img src="src/img/home/rest.jpg" alt="restaurant">
+            </div>
+
+            <div class="rest-text">
+                <h5>Restaurant</h5>
+
+                <h4>Get Restaurant Facilities & Many Other More</h4>
+                <p>
+                    Lorem ipsum dolor sit amet, consectetur adipisicing elit, sed 
+                    do eiusmod tem por incididunt ut labore et dolore magna aliqua. 
+                    Ut enim ad minim veniam, quis nostrud exercitation ullamco 
+                    laboris nisi ut aliquip.
+                </p>
+
+                <button>Take a Tour</button>
+            </div>
+        </div>
+    </div>
+
+    <div class="about__facilities">
+        
+        <h5>Facilities</h5>
+
+        <h2>Core Features</h2>
+
+        <!-- Swiper -->
+        <div class="swiper DataSwiper facilities-mobile">
+            <div class="swiper-wrapper">
+                <div class="swiper-slide">
+                    <img class="number" src="src/img/icons/n1b.svg" alt="01b">
+
+                    <img class="icon" src="src/img/icons/01.svg" alt="Rating">
+
+                    <h4>Have High Rating</h4>
+                    <p>Lorem ipsum dolor sit amet, consectetur adipisicing elit, sed do eiusmod tempor incididunt ut labore et dolore magna..</p>
+                </div>
+
+                <div class="swiper-slide">
+                    <img class="number" src="src/img/icons/n2b.svg" alt="02b">
+
+                    <img class="icon" src="src/img/icons/02.svg" alt="quiet hours">
+
+                    <h4>Quiet Hours</h4>
+                    <p>Lorem ipsum dolor sit amet, consectetur adipisicing elit, sed do eiusmod tempor incididunt ut labore et dolore magna..</p>
+                </div>
+
+                <div class="swiper-slide">
+                    <img class="number" src="src/img/icons/n3b.svg" alt="03b">
+
+                    <img class="icon" src="src/img/icons/03.svg" alt="Locations">
+
+                    <h4>Best Locations</h4>
+                    <p>Lorem ipsum dolor sit amet, consectetur adipisicing elit, sed do eiusmod tempor incididunt ut labore et dolore magna..</p>
+                </div>
+
+                <div class="swiper-slide">
+                    <img class="number" src="src/img/icons/n4b.svg" alt="04b">
+
+                    <img class="icon" src="src/img/icons/04.svg" alt="Cancellation">
+
+                    <h4>Free Cancellation</h4>
+                    <p>Lorem ipsum dolor sit amet, consectetur adipisicing elit, sed do eiusmod tempor incididunt ut labore et dolore magna..</p>
+                </div>
+
+                <div class="swiper-slide">
+                    <img class="number" src="src/img/icons/n5b.svg" alt="05b">
+
+                    <img class="icon" src="src/img/icons/05.svg" alt="Payment Options">
+
+                    <h4>Payment Options</h4>
+                    <p>Lorem ipsum dolor sit amet, consectetur adipisicing elit, sed do eiusmod tempor incididunt ut labore et dolore magna..</p>
+                </div>
+
+                <div class="swiper-slide">
+                    <img class="number" src="src/img/icons/n6b.svg" alt="06b">
+
+                    <img class="icon" src="src/img/icons/06.svg" alt="Special Offers">
+
+                    <h4>Special Offers</h4>
+                    <p>Lorem ipsum dolor sit amet, consectetur adipisicing elit, sed do eiusmod tempor incididunt ut labore et dolore magna..</p>
+                </div>
+
+            </div>
+            <div class="swiper-pagination"></div>
+        </div>
+            
+        <div class="facilities-desktop">
+            <div class="facilities-items">
+                <img class="number" src="src/img/icons/n1b.svg" alt="01b">
+
+                <img class="icon" src="src/img/icons/01.svg" alt="Rating">
+
+                <h4>Have High Rating</h4>
+                <p>Lorem ipsum dolor sit amet, consectetur adipisicing elit, sed do eiusmod tempor incididunt ut labore et dolore magna..</p>
+            </div>
+
+            <div class="facilities-items">
+                <img class="number" src="src/img/icons/n2b.svg" alt="02b">
+
+                <img class="icon" src="src/img/icons/02.svg" alt="quiet hours">
+
+                <h4>Quiet Hours</h4>
+                <p>Lorem ipsum dolor sit amet, consectetur adipisicing elit, sed do eiusmod tempor incididunt ut labore et dolore magna..</p>
+            </div>
+
+            <div class="facilities-items">
+                <img class="number" src="src/img/icons/n3b.svg" alt="03b">
+
+                <img class="icon" src="src/img/icons/03.svg" alt="Locations">
+
+                <h4>Best Locations</h4>
+                <p>Lorem ipsum dolor sit amet, consectetur adipisicing elit, sed do eiusmod tempor incididunt ut labore et dolore magna..</p>
+            </div>
+
+            <div class="facilities-items">
+                <img class="number" src="src/img/icons/n4b.svg" alt="04b">
+
+                <img class="icon" src="src/img/icons/04.svg" alt="Cancellation">
+
+                <h4>Free Cancellation</h4>
+                <p>Lorem ipsum dolor sit amet, consectetur adipisicing elit, sed do eiusmod tempor incididunt ut labore et dolore magna..</p>
+            </div>
+
+            <div class="facilities-items">
+                <img class="number" src="src/img/icons/n5b.svg" alt="05b">
+
+                <img class="icon" src="src/img/icons/05.svg" alt="Payment Options">
+
+                <h4>Payment Options</h4>
+                <p>Lorem ipsum dolor sit amet, consectetur adipisicing elit, sed do eiusmod tempor incididunt ut labore et dolore magna..</p>
+            </div>
+
+            <div class="facilities-items">
+                <img class="number" src="src/img/icons/n6b.svg" alt="06b">
+
+                <img class="icon" src="src/img/icons/06.svg" alt="Special Offers">
+
+                <h4>Special Offers</h4>
+                <p>Lorem ipsum dolor sit amet, consectetur adipisicing elit, sed do eiusmod tempor incididunt ut labore et dolore magna..</p>
+            </div>
+        </div>
+
+    </div>
+
+    <div class="about__counter">
+
+        <h5>Counter</h5>
+
+        <h4>Some Fun Facts</h4>
+
+        <div class="counter__content">
+            <div class="counter">
+                <img src="src/img/icons/person.svg" alt="person">
+
+                <div>
+                    <h3>8000</h3>
+                    <p>Happy Users</p>
+                </div>
+
+                <svg width="32" height="16" viewBox="0 0 32 16" fill="none" xmlns="http://www.w3.org/2000/svg">
+                    <path d="M31.6334 7.11518C31.633 7.11481 31.6327 7.11437 31.6322 7.114L25.1007 0.613996C24.6114 0.127058 23.82 0.128871 23.3329 0.618245C22.8459 1.10756 22.8478 1.899 23.3371 2.386L27.7224 6.74999H1.25C0.559625 6.74999 0 7.30962 0 7.99999C0 8.69037 0.559625 9.24999 1.25 9.24999H27.7223L23.3372 13.614C22.8479 14.101 22.846 14.8924 23.333 15.3817C23.8201 15.8712 24.6116 15.8729 25.1008 15.386L31.6323 8.88599C31.6327 8.88562 31.633 8.88518 31.6334 8.88481C32.123 8.39618 32.1214 7.60218 31.6334 7.11518Z" fill="#DCDCDC"/>
+                </svg>
+            </div>
+
+            <div class="counter">
+                <img src="src/img/icons/good.svg" alt="good">
+
+                <div>
+                    <h3>10M</h3>
+                    <p>Reviews & Appriciate</p>
+                </div>
+
+                <svg width="32" height="16" viewBox="0 0 32 16" fill="none" xmlns="http://www.w3.org/2000/svg">
+                    <path d="M31.6334 7.11518C31.633 7.11481 31.6327 7.11437 31.6322 7.114L25.1007 0.613996C24.6114 0.127058 23.82 0.128871 23.3329 0.618245C22.8459 1.10756 22.8478 1.899 23.3371 2.386L27.7224 6.74999H1.25C0.559625 6.74999 0 7.30962 0 7.99999C0 8.69037 0.559625 9.24999 1.25 9.24999H27.7223L23.3372 13.614C22.8479 14.101 22.846 14.8924 23.333 15.3817C23.8201 15.8712 24.6116 15.8729 25.1008 15.386L31.6323 8.88599C31.6327 8.88562 31.633 8.88518 31.6334 8.88481C32.123 8.39618 32.1214 7.60218 31.6334 7.11518Z" fill="#DCDCDC"/>
+                </svg>
+            </div>
+
+            <div class="counter">
+                <img src="src/img/icons/world.svg" alt="world">
+
+                <div>
+                    <h3>100</h3>
+                    <p>Country Coverage</p>
+                </div>
+
+                <svg width="32" height="16" viewBox="0 0 32 16" fill="none" xmlns="http://www.w3.org/2000/svg">
+                    <path d="M31.6334 7.11518C31.633 7.11481 31.6327 7.11437 31.6322 7.114L25.1007 0.613996C24.6114 0.127058 23.82 0.128871 23.3329 0.618245C22.8459 1.10756 22.8478 1.899 23.3371 2.386L27.7224 6.74999H1.25C0.559625 6.74999 0 7.30962 0 7.99999C0 8.69037 0.559625 9.24999 1.25 9.24999H27.7223L23.3372 13.614C22.8479 14.101 22.846 14.8924 23.333 15.3817C23.8201 15.8712 24.6116 15.8729 25.1008 15.386L31.6323 8.88599C31.6327 8.88562 31.633 8.88518 31.6334 8.88481C32.123 8.39618 32.1214 7.60218 31.6334 7.11518Z" fill="#DCDCDC"/>
+                </svg>
+            </div>
+
+        </div>
+
+
+        <!-- Swiper -->
+        <div class="swiper MenuSwiper count-mobile">
+            <div class="swiper-wrapper">
+                <div class="swiper-slide">
+                    <img src="src/img/home/about1.jpg" alt="count">
+                </div>
+
+                <div class="swiper-slide">
+                    <img src="src/img/home/about2.jpg" alt="count">
+                </div>
+
+                <div class="swiper-slide">
+                    <img src="src/img/home/rest.jpg" alt="count">
+                </div>
+            </div>
+            <div class="swiper-pagination"></div>
+        </div>
+
+        <div class="count-desktop">
+            <div class="count-img">
+                <img src="src/img/home/about1.jpg" alt="count">
+            </div>
+
+            <div class="count-img">
+                <img src="src/img/home/about2.jpg" alt="count">
+            </div>
+        </div>
+    </div>
+
+</div>
+@endsection
